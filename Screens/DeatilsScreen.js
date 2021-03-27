8776803f9d23f0c73e88b95314ee445fbc41c316
fullsnack-DEV <pratikpatre2019@gmail.com>
2021-03-27 18:28:39 +0530

    implementing a Shared Element Transition

--- Screens/DeatilsScreen.js
+++ Screens/DeatilsScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
+//To implemnt a React native Shared Element Transition
+
 export default function DeatilsScreen({route}) {
   let {item} = route.params;
 
